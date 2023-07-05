02ac3a4b0ee5a65cd896cf630b90c7efc6896b33
Your Name <sarah.moreira@neon.com.br>
2023-07-05 14:47:17 -0300

    html Score

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+function finalizar(){
+    const item1 = parseInt(document.getElementById("item1").value);
+    const item2 = parseInt(document.getElementById("item2").value);
+    const item3 = parseInt(document.getElementById("item3").value);
+    const item4 = parseInt(document.getElementById("item4").value);
+    const item5 = parseInt(document.getElementById("item5").value);
+    const item6 = parseInt(document.getElementById("item6").value);
+    const item7 = parseInt(document.getElementById("item7").value);
+    const item8 = parseInt(document.getElementById("item8").value);
+    const item9 = parseInt(document.getElementById("item9").value);
+    const somar = item1 + item2 + item3 + item4 + item5 + item6 + item7 + item8 + item9;
+    alert(somar);
+}
